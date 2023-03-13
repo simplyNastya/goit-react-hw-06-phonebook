3eb96cbb7ef2bc1165ccf1af2f2441ef3da476ac
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-03-13 21:20:05 +0100

    Refactored code

--- src/redux/store.js
+++ src/redux/store.js
@@ -22,4 +22,4 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-export default store
+export default store;
